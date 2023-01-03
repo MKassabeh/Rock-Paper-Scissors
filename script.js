5e6283dec83cef4db112f683251ca2d32425e931
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-03 11:59:07 +0100

    final function rock paper scissors

--- script.js
+++ script.js
@@ -3,51 +3,57 @@ console.log("Hello world!");
 
 
 
-function getComputerChoice(x) {
 
-    var randValue = Math.floor(Math.random() * 3);
-    var choise = ["Rock","Paper","Scissors"];
-    return choise[randValue];
+for (let index = 0; index < 5; index++) {
+
+    
+    function singleRound(x,y){
+        if(x === y){
+            console.log("It's a tie");
+        }
+        else if(x === "rock"){
+            if(y === "scissors"){
+                console.log("You win");
+            }
+            else{
+                console.log("You lose");
+            }
+        }
+        else if(x === "paper"){
+            if(y === "rock"){
+                console.log("You Win");
+            }
+            else{
+                console.log("You Lose");
+            }
+        }
+        else if(x === "scissors"){
+            if(y === "paper"){
+                console.log("You Win");
+            }
+            else{
+                console.log("You Win");
+            }
+        }
+    };
+    
+    function getComputerChoice(x) {
+    
+        var randValue = Math.floor(Math.random() * 3);
+        var choise = ["Rock","Paper","Scissors"];
+        return choise[randValue];
+    }
+    
+    var computerSelection = getComputerChoice().toLowerCase();
+    
+    var userSelection =  prompt("Rock , Paper , Scissors").toLowerCase();
+    
+    console.log("computer choose : "+computerSelection)
+
+    console.log("user choose : "+userSelection)
+    
+    singleRound(userSelection,computerSelection);
+    
+
+    
 }
-
-var computerSelection = getComputerChoice().toLowerCase();
-
-console.log(computerSelection);
-
-var userSelection =  prompt("Rock , Paper , Scissors").toLowerCase()
-console.log(userSelection);
-
-
-
-function singleRound(x,y){
-    if(x === y){
-        console.log("It's a tie");
-    }
-    else if(x === "rock"){
-        if(y === "scissors"){
-            console.log("You win");
-        }
-        else{
-            console.log("You lose");
-        }
-    }
-    else if(x === "paper"){
-        if(y === "rock"){
-            console.log("You Win");
-        }
-        else{
-            console.log("You Lose");
-        }
-    }
-    else if(x === "scissors"){
-        if(y === "paper"){
-            console.log("You Win");
-        }
-        else{
-            console.log("You Win");
-        }
-    }
-}
-
-
-singleRound(userSelection,computerSelection);
