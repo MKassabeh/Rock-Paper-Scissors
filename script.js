305c2216a2deac0608d40ac7719cbdfc9e968484
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-03 11:44:03 +0100

    end of the function singleRound

--- script.js
+++ script.js
@@ -10,8 +10,44 @@ function getComputerChoice(x) {
     return choise[randValue];
 }
 
-var computerSelection = getComputerChoice();
-
-var userSelection = prompt("What do you play : Rock , Paper , Scissors")
+var computerSelection = getComputerChoice().toLowerCase();
 
 console.log(computerSelection);
+
+var userSelection =  prompt("Rock , Paper , Scissors").toLowerCase()
+console.log(userSelection);
+
+
+
+function singleRound(x,y){
+    if(x === y){
+        console.log("It's a tie");
+    }
+    else if(x === "rock"){
+        if(y === "scissors"){
+            console.log("You win");
+        }
+        else{
+            console.log("You lose");
+        }
+    }
+    else if(x === "paper"){
+        if(y === "rock"){
+            console.log("You Win");
+        }
+        else{
+            console.log("You Lose");
+        }
+    }
+    else if(x === "scissors"){
+        if(y === "paper"){
+            console.log("You Win");
+        }
+        else{
+            console.log("You Win");
+        }
+    }
+}
+
+
+singleRound(userSelection,computerSelection);
