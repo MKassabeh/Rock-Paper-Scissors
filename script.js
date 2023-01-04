6c58c7e4e8170bb2fbf183eb653028cee21657dc
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-04 14:35:30 +0100

    rock paper scissors dom function finish

--- script.js
+++ script.js
@@ -1,52 +1,62 @@
 //Select all buttons
-const buttons = document.querySelectorAll("button")
+const buttons = document.querySelectorAll("button");
+
+const result = document.querySelector("#result");
+
+const showResult = document.createElement("div");
+
+const resultFight = document.createElement("div");;
 
 //For each button click event
 buttons.forEach(button => {
     button.addEventListener("click", function playRound(){
+        //function for the computer choice
+        function getComputerChoice(x) {
+
+            var randValue = Math.floor(Math.random() * 3);
+            var choise = ["Rock","Paper","Scissors"];
+            return choise[randValue];
+        }
+        var computerChoice = getComputerChoice().toLowerCase();
+        if(button.getAttribute("id") === "rock" ){
+            resultFight.innerHTML ="<span>User choicie : Rock Computer Choice : "+computerChoice+"</span>"
+            if(computerChoice==="paper"){
+                showResult.innerHTML ="<span>You lose</span>"
+            }else if(computerChoice==="scissors"){
+                showResult.innerHTML ="<span>You win</span>"
+            }else{
+                showResult.innerHTML ="<span>It's a tie</span>"
+
+            }
+
+        }else if(button.getAttribute("id") === "paper"){
+            resultFight.innerHTML ="<span>User choicie : Paper Computer Choice : "+computerChoice+"</span>"
+            if(computerChoice==="scissors"){
+                showResult.innerHTML ="<span>You lose</span>"
+            }else if(computerChoice==="rock"){
+                showResult.innerHTML ="<span>You win</span>"
+            }else{
+                showResult.innerHTML ="<span>It's a tie</span>"
+
+            }
+
+        }else{
+            resultFight.innerHTML ="<span>User choicie : Scissors Computer Choice : "+computerChoice+"</span>"
+            if(computerChoice==="rock"){
+                showResult.innerHTML ="<span>You lose</span>"
+            }else if(computerChoice==="paper"){
+                showResult.innerHTML ="<span>You win</span>"
+            }else{
+                showResult.innerHTML ="<span>It's a tie</span>"
+
+            }
+
+        }
+        result.appendChild(resultFight);
+        result.appendChild(showResult);
         
         })
+        
     }
 )
-
-//function for the computer choice
-function getComputerChoice(x) {
-
-    var randValue = Math.floor(Math.random() * 3);
-    var choise = ["Rock","Paper","Scissors"];
-    return choise[randValue];
-}
-    
-
-
-//Functon to check who win or lose round
-function singleRound(x,y){
-    if(x === y){
-        console.log("It's a tie");
-    }
-    else if(x === "rock"){
-        if(y === "scissors"){
-            console.log("You win");
-        }
-        else{
-            console.log("You lose");
-        }
-    }
-    else if(x === "paper"){
-        if(y === "rock"){
-            console.log("You Win");
-        }
-        else{
-            console.log("You Lose");
-        }
-    }
-    else if(x === "scissors"){
-        if(y === "paper"){
-            console.log("You Win");
-        }
-        else{
-            console.log("You Win");
-        }
-    }
-};
 
