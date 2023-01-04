278bda4480b2a824d7b532465c500c2d7e5157f8
Mkassabeh <marwane_kassabeh@outlook.fr>
2023-01-04 13:40:41 +0100

    commentary ,indentation and refactoring code using DOM

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
-console.log("Hello world!");
+//Select all buttons
+const buttons = document.querySelectorAll("button")
 
-
-
-
-
-for (let index = 0; index < 5; index++) {
-
-    
-    function singleRound(x,y){
-        if(x === y){
-            console.log("It's a tie");
-        }
-        else if(x === "rock"){
-            if(y === "scissors"){
-                console.log("You win");
-            }
-            else{
-                console.log("You lose");
-            }
-        }
-        else if(x === "paper"){
-            if(y === "rock"){
-                console.log("You Win");
-            }
-            else{
-                console.log("You Lose");
-            }
-        }
-        else if(x === "scissors"){
-            if(y === "paper"){
-                console.log("You Win");
-            }
-            else{
-                console.log("You Win");
-            }
-        }
-    };
-    
-    function getComputerChoice(x) {
-    
-        var randValue = Math.floor(Math.random() * 3);
-        var choise = ["Rock","Paper","Scissors"];
-        return choise[randValue];
+//For each button click event
+buttons.forEach(button => {
+    button.addEventListener("click", function playRound(){
+        
+        })
     }
-    
-    var computerSelection = getComputerChoice().toLowerCase();
-    
-    var userSelection =  prompt("Rock , Paper , Scissors").toLowerCase();
-    
-    console.log("computer choose : "+computerSelection)
+)
 
-    console.log("user choose : "+userSelection)
-    
-    singleRound(userSelection,computerSelection);
-    
+//function for the computer choice
+function getComputerChoice(x) {
 
-    
+    var randValue = Math.floor(Math.random() * 3);
+    var choise = ["Rock","Paper","Scissors"];
+    return choise[randValue];
 }
+    
+
+
+//Functon to check who win or lose round
+function singleRound(x,y){
+    if(x === y){
+        console.log("It's a tie");
+    }
+    else if(x === "rock"){
+        if(y === "scissors"){
+            console.log("You win");
+        }
+        else{
+            console.log("You lose");
+        }
+    }
+    else if(x === "paper"){
+        if(y === "rock"){
+            console.log("You Win");
+        }
+        else{
+            console.log("You Lose");
+        }
+    }
+    else if(x === "scissors"){
+        if(y === "paper"){
+            console.log("You Win");
+        }
+        else{
+            console.log("You Win");
+        }
+    }
+};
+
